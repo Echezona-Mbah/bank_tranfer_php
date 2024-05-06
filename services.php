9cdce5cf67abfd27efd123b438cc3236a29db6c4
Echezona-Mbah <cclassico998@gmail.com>
2024-05-06 15:38:15 -0700

    update new function

--- services.php
+++ services.php
@@ -19,8 +19,8 @@
             <div class="container text-center py-5" style="max-width: 900px;">
                 <h3 class="text-white display-3 mb-4">Our Services</h1>
                 <ol class="breadcrumb justify-content-center mb-0">
-                    <li class="breadcrumb-item"><a href="index.html">Home</a></li>
-                    <li class="breadcrumb-item"><a href="#">Pages</a></li>
+                    <!-- <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                    <li class="breadcrumb-item"><a href="#">Pages</a></li> -->
                     <li class="breadcrumb-item active text-white">Services</li>
                 </ol>    
             </div>
@@ -40,8 +40,8 @@
                             <div class="col-12">
                                 <div class="service-content-inner d-flex align-items-center bg-white border border-primary rounded p-4 pe-0">
                                     <div class="service-content text-end">
-                                        <h5 class="mb-4">WorldWide Tours</h5>
-                                        <p class="mb-0">Dolor sit amet consectetur adipisicing elit. Non alias eum, suscipit expedita corrupti officiis debitis possimus nam laudantium beatae quidem dolore consequuntur voluptate rem reiciendis, omnis sequi harum earum.
+                                        <h5 class="mb-4">Savings Account</h5>
+                                        <p class="mb-0">Whether you want instant access to your cash, a fixed rate or an account you can manage easily from the comfort of your living room, we have a saving account suited for you.
                                         </p>
                                     </div>
                                     <div class="service-icon p-4">
@@ -52,9 +52,8 @@
                             <div class="col-12">
                                 <div class="service-content-inner d-flex align-items-center  bg-white border border-primary rounded p-4 pe-0">
                                     <div class="service-content text-end">
-                                        <h5 class="mb-4">Hotel Reservation</h5>
-                                        <p class="mb-0">Dolor sit amet consectetur adipisicing elit. Non alias eum, suscipit expedita corrupti officiis debitis possimus nam laudantium beatae quidem dolore consequuntur voluptate rem reiciendis, omnis sequi harum earum.
-                                        </p>
+                                        <h5 class="mb-4">Current Account</h5>
+                                        <p class="mb-0">Are you looking for something beyond a simple transactional account? We offer a range of current accounts to suit everyone from the young graduate to the upwardly mobile professional.                                        </p>
                                     </div>
                                     <div class="service-icon p-4">
                                         <i class="fa fa-hotel fa-4x text-primary"></i>
@@ -64,8 +63,8 @@
                             <div class="col-12">
                                 <div class="service-content-inner d-flex align-items-center bg-white border border-primary rounded p-4 pe-0">
                                     <div class="service-content text-end">
-                                        <h5 class="mb-4">Travel Guides</h5>
-                                        <p class="mb-0">Dolor sit amet consectetur adipisicing elit. Non alias eum, suscipit expedita corrupti officiis debitis possimus nam laudantium beatae quidem dolore consequuntur voluptate rem reiciendis, omnis sequi harum earum.
+                                        <h5 class="mb-4">Diaspora Banking</h5>
+                                        <p class="mb-0">Coastchartered in Diaspora no longer have to worry about conducting transactions in their home country. We provide them with a wide range of stress-free, easy-to-use and convenient banking services.y.
                                         </p>
                                     </div>
                                     <div class="service-icon p-4">
@@ -73,18 +72,7 @@
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-12">
-                                <div class="service-content-inner d-flex align-items-center bg-white border border-primary rounded p-4 pe-0">
-                                    <div class="service-content text-end">
-                                        <h5 class="mb-4">Event Management</h5>
-                                        <p class="mb-0">Dolor sit amet consectetur adipisicing elit. Non alias eum, suscipit expedita corrupti officiis debitis possimus nam laudantium beatae quidem dolore consequuntur voluptate rem reiciendis, omnis sequi harum earum.
-                                        </p>
-                                    </div>
-                                    <div class="service-icon p-4">
-                                        <i class="fa fa-cog fa-4x text-primary"></i>
-                                    </div>
-                                </div>
-                            </div>
+                        
                         </div>
                     </div>
                     <div class="col-lg-6">
@@ -95,8 +83,8 @@
                                         <i class="fa fa-globe fa-4x text-primary"></i>
                                     </div>
                                     <div class="service-content">
-                                        <h5 class="mb-4">WorldWide Tours</h5>
-                                        <p class="mb-0">Dolor sit amet consectetur adipisicing elit. Non alias eum, suscipit expedita corrupti officiis debitis possimus nam laudantium beatae quidem dolore consequuntur voluptate rem reiciendis, omnis sequi harum earum.
+                                        <h5 class="mb-4">100% secure</h5>
+                                        <p class="mb-0">Credit Card such as Master Card and Visa payments are performed under an encrypted network and a secure method of transmission provided by our partner CIC bank, via its Monetico Payment solution.
                                         </p>
                                     </div>
                                 </div>
@@ -107,8 +95,8 @@
                                         <i class="fa fa-hotel fa-4x text-primary"></i>
                                     </div>
                                     <div class="service-content">
-                                        <h5 class="mb-4">Hotel Reservation</h5>
-                                        <p class="mb-0">Dolor sit amet consectetur adipisicing elit. Non alias eum, suscipit expedita corrupti officiis debitis possimus nam laudantium beatae quidem dolore consequuntur voluptate rem reiciendis, omnis sequi harum earum.
+                                        <h5 class="mb-4">Personal Loans</h5>
+                                        <p class="mb-0">Customized loans designed to meet your specific personal needs. Get funded for personal loans, lines of credit, payment cards and other types of funding regardless of your credit situation.
                                         </p>
                                     </div>
                                 </div>
@@ -119,24 +107,13 @@
                                         <i class="fa fa-user fa-4x text-primary"></i>
                                     </div>
                                     <div class="service-content">
-                                        <h5 class="mb-4">Travel Guides</h5>
-                                        <p class="mb-0">Dolor sit amet consectetur adipisicing elit. Non alias eum, suscipit expedita corrupti officiis debitis possimus nam laudantium beatae quidem dolore consequuntur voluptate rem reiciendis, omnis sequi harum earum.
+                                        <h5 class="mb-4">Money Transfer</h5>
+                                        <p class="mb-0">Make local and international money transfer easily and safely. Our superior technology platform and robust partnership ecosystem enables us deliver unrivaled service to customers.
                                         </p>
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-12">
-                                <div class="service-content-inner d-flex align-items-center bg-white border border-primary rounded p-4 ps-0">
-                                    <div class="service-icon p-4">
-                                        <i class="fa fa-cog fa-4x text-primary"></i>
-                                    </div>
-                                    <div class="service-content">
-                                        <h5 class="mb-4">Event Management</h5>
-                                        <p class="mb-0">Dolor sit amet consectetur adipisicing elit. Non alias eum, suscipit expedita corrupti officiis debitis possimus nam laudantium beatae quidem dolore consequuntur voluptate rem reiciendis, omnis sequi harum earum.
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
+
                         </div>
                     </div>
                     <div class="col-12">
@@ -154,12 +131,12 @@
             <div class="container py-5">
                 <div class="mx-auto text-center mb-5" style="max-width: 900px;">
                     <h5 class="section-title px-3">Testimonial</h5>
-                    <h1 class="mb-0">Our Clients Say!!!</h1>
+                    <h1 class="mb-0">Read what our outstanding clients have to say</h1>
                 </div>
                 <div class="testimonial-carousel owl-carousel">
                     <div class="testimonial-item text-center rounded pb-4">
                         <div class="testimonial-comment bg-light rounded p-4">
-                            <p class="text-center mb-5">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis nostrum cupiditate, eligendi repellendus saepe illum earum architecto dicta quisquam quasi porro officiis. Vero reiciendis,
+                            <p class="text-center mb-5">I am pleased to see Coastchartered Private Banking brand wax stronger over the years. This is quite heart warming. Their in-depth understanding of the Private Banking business has placed them in a very strong competitive position,
                             </p>
                         </div>
                         <div class="testimonial-img p-1">
@@ -179,14 +156,14 @@
                     </div>
                     <div class="testimonial-item text-center rounded pb-4">
                         <div class="testimonial-comment bg-light rounded p-4">
-                            <p class="text-center mb-5">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis nostrum cupiditate, eligendi repellendus saepe illum earum architecto dicta quisquam quasi porro officiis. Vero reiciendis,
+                            <p class="text-center mb-5">As one of the pioneer clients of Coastchartered Private Banking, I am glad to be part of it’s well-deserved 20th Anniversary celebration having shown consistency and stability.
                             </p>
                         </div>
                         <div class="testimonial-img p-1">
                             <img src="img/testimonial-2.jpg" class="img-fluid rounded-circle" alt="Image">
                         </div>
                         <div style="margin-top: -35px;">
-                            <h5 class="mb-0">John Abraham</h5>
+                            <h5 class="mb-0">Amelia Audrey</h5>
                             <p class="mb-0">New York, USA</p>
                             <div class="d-flex justify-content-center">
                                 <i class="fas fa-star text-primary"></i>
@@ -199,14 +176,15 @@
                     </div>
                     <div class="testimonial-item text-center rounded pb-4">
                         <div class="testimonial-comment bg-light rounded p-4">
-                            <p class="text-center mb-5">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis nostrum cupiditate, eligendi repellendus saepe illum earum architecto dicta quisquam quasi porro officiis. Vero reiciendis,
+                            <p class="text-center mb-5">Banking for over 15 years, during which time I have enjoyed good quality service.
+Staff are courteous, knowledgeable and always ready to proffer solutions to complex problems.,
                             </p>
                         </div>
                         <div class="testimonial-img p-1">
                             <img src="img/testimonial-3.jpg" class="img-fluid rounded-circle" alt="Image">
                         </div>
                         <div style="margin-top: -35px;">
-                            <h5 class="mb-0">John Abraham</h5>
+                            <h5 class="mb-0">Lily Ava</h5>
                             <p class="mb-0">New York, USA</p>
                             <div class="d-flex justify-content-center">
                                 <i class="fas fa-star text-primary"></i>
@@ -219,14 +197,14 @@
                     </div>
                     <div class="testimonial-item text-center rounded pb-4">
                         <div class="testimonial-comment bg-light rounded p-4">
-                            <p class="text-center mb-5">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis nostrum cupiditate, eligendi repellendus saepe illum earum architecto dicta quisquam quasi porro officiis. Vero reiciendis,
+                            <p class="text-center mb-5">Coastchartered Private Banking; an institution that captures the true essence of Family. It is a pleasure to work with a Bank that not only understands our firm but also our industry’s specific needs. The Managing Director, Executive Directors and my Relationship officer have been wonderful.,
                             </p>
                         </div>
                         <div class="testimonial-img p-1">
                             <img src="img/testimonial-4.jpg" class="img-fluid rounded-circle" alt="Image">
                         </div>
                         <div style="margin-top: -35px;">
-                            <h5 class="mb-0">John Abraham</h5>
+                            <h5 class="mb-0">Jose Mary</h5>
                             <p class="mb-0">New York, USA</p>
                             <div class="d-flex justify-content-center">
                                 <i class="fas fa-star text-primary"></i>
